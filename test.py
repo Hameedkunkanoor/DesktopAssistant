5588e46d298dec0536696cc2b12a041c579cbdf2
Hameedkunkanoor <41198503+Hameedkunkanoor@users.noreply.github.com>
2019-11-12 12:23:49 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,18 +1,25 @@
-import speech_recognition as sr  
+import speech_recognition as sr
 import webbrowser
-# get audio from the microphone                                                                       
-r = sr.Recognizer()     
+import os
+import sys
+# get audio from the microphone
+r = sr.Recognizer()
 
 def assistant(command):
-    "if statements for executing commands"
-#open subreddit Reddit
-    if 'notepad' in command:
+
+
+    if 'Notepad' in command:
        os.system("notepad")
-        
+
     elif 'shutdown' in command:
-        sofiaResponse('Bye bye Sir. Have a nice day')
-        sys.exit()
-#open website
+        SpeakText('Bye  Bro. Have a nice day')
+        print("Bye  Bro. Have a nice day")
+        try:
+         os._exit(0)
+        except:
+         print("done")
+
+
     elif 'open' in command:
         #reg_ex = re.search('open (.+)', command)
         #if reg_ex:
@@ -20,19 +27,48 @@ def assistant(command):
         #    print(domain)
         url = 'https://www.' + command
         webbrowser.open(url)
-            
-        
+
+    elif 'chrome' in command:
+        url = 'https://www.' + command
+        chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+        webbrowser.get(chrome_path).open(url)
 
     elif 'notepad' in command:
         os.system("notepad")
 
 
+def StartListening(ted):
+
+ try:
+
+
+    re="You said " + r.recognize_google(ted)
+    if 'listen' in re:
+     print("Listening Buddy")
+     SpeakText("Listening Buddy")
+     #SpeakText(re)
+     with sr.Microphone() as source:
+       r.adjust_for_ambient_noise(source, duration=0.5)
+       audio2 = r.listen(source)
+     StartListening(audio2)
+     SpeakText(r.recognize_google(audio2))
+     assistant(r.recognize_google(audio2))
+     startListen()
+
+ except sr.RequestError as e:
+    print("Could not request results; {0}".format(e))
+ except sr.UnknownValueError:
+     print("",end="")
+
+
+
+
+
+
+
 
 
 import os
-os.system("echo 'hello world'")
-
-
 from win32com.client import Dispatch
 
 speak = Dispatch("SAPI.SpVoice")
@@ -40,20 +76,12 @@ def SpeakText(command):
     speak.Speak(command)
 
 
-while(1):                                                    
- with sr.Microphone() as source:             
-     r.adjust_for_ambient_noise(source, duration=3)                                                            
-     print("Speak:")                                                                                   
-     audio = r.listen(source)   
-    
- try:
-    #sofiaResponse(audio)
-    print("You said " + r.recognize_google(audio))
-    re="You said " + r.recognize_google(audio)
-    SpeakText(re)
-    assistant(r.recognize_google(audio))
- except sr.UnknownValueError:
-    print("Could not understand audio")
- except sr.RequestError as e:
-    print("Could not request results; {0}".format(e))
+def startListen():
+ while(1):
+  with sr.Microphone() as source:
+     r.adjust_for_ambient_noise(source, duration=0.5)
+     #print("Speak:")
+     audio = r.listen(source)
+     StartListening(audio)
 
+startListen()
